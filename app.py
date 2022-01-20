19442b62437e9c71393f18705fc680d14c99954e
Okoth Pius <orukopius8@gmail.com>
2022-01-20 23:48:42 +0300

    feat: update file

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/super_simple')
+def super_simple():
+    return 'Hello from Planetary API'
+
+if __name__ == '__main__':
+    app.run()
