9517a1bd828a70aee1c931d3e5131f12b24d9ac7
Okoth Pius <orukopius8@gmail.com>
2022-01-22 12:52:17 +0300

    [perf] Clean up returned code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -10,6 +10,29 @@ def hello_world():
 @app.route('/super_simple')
 def super_simple():
     return jsonify(message='Hello from Planetary API')
+
+
+@app.route('/not_found')
+def not_found():
+    return jsonify(message='Not found'), 404
+
+
+@app.route('/parameters')
+def parameters():
+    name = request.args.get('name')
+    age = int(request.args.get('age'))
+
+    if age < 18:
+        return jsonify(message='Sorry ' + name + ', youre not old enough!'), 401
+    return jsonify(message='Youre are welcome ' + name)
+
+
+@app.route('/url_variables/<string:name>/<int:age>')
+def url_variables(name: str, age: int):
+    if age < 18:
+        return jsonify(message='Sorry ' + name + ', youre not old enough!'), 401
+    return jsonify(message='Youre are welcome ' + name)
+
 
 if __name__ == '__main__':
     app.run()
