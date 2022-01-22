abf02bbefe7e7f4ccbe79c2d03da986b07ba7474
Okoth Pius <orukopius8@gmail.com>
2022-01-22 17:24:16 +0300

    [chore] update jwt

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 import os
-from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String, Float
 from flask import Flask, jsonify, request
+from flask_marshmallow import Marshmallow
+from sqlalchemy import Column, Integer, String, Float
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
 
 # Configurations
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'planets.db')
+app.config['JWT_SECRET_KEY'] = '<secret_key>'
+
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+jwt = JWTManager(app)
 
-# Commands
+# Database commands
 @app.cli.command('db_create')
 def db_create():
     db.create_all()
@@ -97,6 +101,38 @@ def planets():
     planets_list = Planet.query.all()
     result = planets_schema.dump(planets_list)
     return jsonify(result)
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    email = request.form['email']
+    test = User.query.filter_by(email=email).first()
+    if test:
+        return jsonify(message='That email already exist!'), 409
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    password = request.form['password']
+    user = User(first_name=first_name, last_name=last_name, email=email,
+                password=password)
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify(message='User has been created Succesfully!'), 201
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.is_json:
+        email = request.json['email']
+        password = request.json['password']
+    email = request.form['email']
+    password = request.form['password']
+
+    test = User.query.filter_by(email=email, password=password).first()
+    if test:
+        access_token = create_access_token(identity=email)
+        return jsonify(message='Login Succeeded!', access_token=access_token)
+    return jsonify(message='Bad email or password!'), 401
 
 
 # Database Models
