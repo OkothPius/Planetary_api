67a5ce5be167bb61cd498520d8c2c70f3cacd79a
Okoth Pius <orukopius8@gmail.com>
2022-01-25 16:00:56 +0300

    [feat: add planet]

--- app.py
+++ app.py
@@ -172,6 +172,7 @@ def planet_details(planet_id: int):
 
 
 @app.route('/add_planet', methods=['POST'])
+@jwt_required()
 def add_planet():
     planet_name = request.form['planet_name']
     test = Planet.query.filter_by(planet_name=planet_name).first()
